3a0d57c0d077fb4c482b984a3f25a524a6d2602f
tecnologiasobras-cyber <tecnologiasobras@gmail.com>
2026-02-24 14:57:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const isProd = process.env.BUILD_MODE === 'prod'
 export default defineConfig({
-  plugins: [
-    react(),
-    sourceIdentifierPlugin({
-      enabled: !isProd,
-      attributePrefix: 'data-matrix',
-      includeProps: true,
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  base: '/sobras-aluminio-dashboard/',
 })
